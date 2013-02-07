97bfb1af43a148497c720db05812c21f3e5b7d65
stonedog <mydba123@gmail.com>
2013-02-07 15:27:15 +0800

    0.1 良好的开始

---
+++ system.monitor/src/stonedog/monitor/common/properties.py
@@ -0,0 +1,26 @@
+'''
+Created on 2013-2-7
+
+@author: Administrator
+'''
+
+class PropertyProxy(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self,propName,propType):
+        '''
+        Constructor
+        '''
+        pass
+    
+    def __set__(self,propValue):
+        pass
+    
+    def __get__(self):
+        pass
+    
+    def __del__(self):
+        pass
---
+++ system.monitor/src/stonedog/monitor/config.py
@@ -0,0 +1,18 @@
+#encoding=utf-8
+'''
+Created on 2013-2-7
+
+@author: Administrator
+'''
+
+class MachineConfig(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(selfparams):
+        '''
+        Constructor
+        '''
+        
